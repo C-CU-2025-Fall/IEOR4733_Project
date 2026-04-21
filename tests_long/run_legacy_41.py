359412ba5f928cf8065fce6796d978f4f701d86e
ly2700-a11y <ly2700@columbia.edu>
2026-04-21 14:02:30 -0400

    更新了data
    更新了data

--- tests_long/run_legacy_41.py
+++ tests_long/run_legacy_41.py
@@ -9,7 +9,7 @@ ROOT = Path(__file__).resolve().parents[1]
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
 
-TESTS = ROOT / "tests"
+TESTS = ROOT / "tests_long"
 if str(TESTS) not in sys.path:
     sys.path.insert(0, str(TESTS))
 
